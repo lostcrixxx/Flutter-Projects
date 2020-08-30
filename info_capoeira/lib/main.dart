3b6f4643ed02bc3419cab9bd6b6b4d47b78c23c5
Cristiano M <devcristianomorais@gmail.com>
2020-08-30 18:37:42 -0300

    implementation onesignal

--- info_capoeira/lib/main.dart
+++ info_capoeira/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:info_capoeira/screens/home_screen.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,6 +8,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    OneSignal.shared.init('ba94e8e8-caf5-4c0e-bfc9-9a5daf91a225');
+
     return MaterialApp(
       title: 'Canal do Youtube',
       debugShowCheckedModeBanner: false,
